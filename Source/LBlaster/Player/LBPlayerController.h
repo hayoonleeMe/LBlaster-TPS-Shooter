57126b16619ea5ab789d29ce2556d40f1935a802
hayoonlee <htdobad@gmail.com>
2024-01-23 22:20:07 +0900

    맵 추가 및 Travel을 Seamless Travel로 변경

---
+++ Source/LBlaster/Player/LBPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LBPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LBLASTER_API ALBPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
