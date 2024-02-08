23ee4f9ca97f7677813477240583682706ac293a
hayoonlee <htdobad@gmail.com>
2024-02-08 19:00:09 +0900

    Locally Controlled Client Character에서 Aiming 시 화면이 떨리는 현상 수정

---
+++ Source/LBlaster/LBTypes/WeaponTypes.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+	EWT_Unarmed UMETA(DisplayName = "Unarmed"),
+	EWT_Pistol UMETA(DisplayName = "Pistol"),
+	EWT_Rifle UMETA(DisplayName = "Rifle"),
+	EWT_Shotgun UMETA(DisplayName = "Shotgun"),
+	EWT_MAX UMETA(DisplayName = "DefaultMax")
+};
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Initial State"),
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+	EWS_MAX UMETA(DisplayName = "DefaultMAX")
+};
