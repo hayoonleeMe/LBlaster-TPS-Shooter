17a330ee998e9140eab7dd4e4d8b83918325365f
hayoonleeMe <htdobad@gmail.com>
2024-03-14 15:03:40 +0900

    Grenade가 덜 튀게 설정 조정

--- Source/LBlaster/Weapon/ThrowableGrenade.cpp
+++ Source/LBlaster/Weapon/ThrowableGrenade.cpp
@@ -3,6 +3,22 @@
 
 #include "Weapon/ThrowableGrenade.h"
 
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AThrowableGrenade::AThrowableGrenade()
+{
+	/* Destroy */
+	DestroyTime = 2.5f;
+
+	/* Projectile Movement */
+	ProjectileMovementComponent->ProjectileGravityScale = 1.3f;
+	ProjectileMovementComponent->Bounciness = 0.4f;
+	ProjectileMovementComponent->Friction = 0.3f;
+
+	/* ThrowableGrenade */
+	DamageExposure = 120.f;
+}
+
 void AThrowableGrenade::BeginPlay()
 {
 	Super::BeginPlay();
