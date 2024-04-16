848dd03b1afa11860b1d4b6defc24bdc73258954
hayoonleeMe <htdobad@gmail.com>
2024-04-17 03:00:06 +0900

    좌, 우로 옵션을 선택할 수 있는 위젯 추가 및 UI에 사용하는 아이콘 폴더 구조 수정

---
+++ Source/LBlaster/HUD/OptionSelector.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD/OptionSelector.h"
+
+#include "LBlaster.h"
+#include "Blueprint/WidgetTree.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/WidgetSwitcher.h"
+
+void UOptionSelector::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	InitializeOptions();
+}
+
+void UOptionSelector::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (LeftButton)
+	{
+		LeftButton->OnClicked.AddDynamic(this, &ThisClass::SelectLeftOption);
+	}
+	if (RightButton)
+	{
+		RightButton->OnClicked.AddDynamic(this, &ThisClass::SelectRightOption);
+	}
+}
+
+void UOptionSelector::InitializeOptions()
+{
+	if (!Switcher || Options.IsEmpty())
+	{
+		return;
+	}
+	
+	for (const FString& Option : Options)
+	{
+		if (UTextBlock* TextBlock = WidgetTree->ConstructWidget<UTextBlock>())
+		{
+			TextBlock->SetText(FText::FromString(Option));
+			TextBlock->SetMinDesiredWidth(400.f);
+			TextBlock->SetJustification(ETextJustify::Center);
+			Switcher->AddChild(TextBlock);
+		}
+	}
+}
+
+void UOptionSelector::SelectRightOption()
+{
+	if (Switcher)
+	{
+		Switcher->SetActiveWidgetIndex(GetNewIndex(1));
+	}
+}
+
+void UOptionSelector::SelectLeftOption()
+{
+	if (Switcher)
+	{
+		Switcher->SetActiveWidgetIndex(GetNewIndex(-1));
+	}
+}
+
+int32 UOptionSelector::GetNewIndex(int32 InAddAmount) const
+{
+	if (Switcher)
+	{
+		const int32 OptionsNum = Options.Num();
+		return (Switcher->GetActiveWidgetIndex() + InAddAmount + OptionsNum) % OptionsNum;
+	}
+	return 0;
+}
