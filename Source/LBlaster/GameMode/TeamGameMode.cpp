b77c2af867d02d4d28d5b2de3a18484a58c78bde
hayoonleeMe <htdobad@gmail.com>
2024-05-20 17:36:03 +0900

    플레이어가 도중에 참가할 때 팀원 수가 적은 팀에 배치되는 기능 구현

--- Source/LBlaster/GameMode/TeamGameMode.cpp
+++ Source/LBlaster/GameMode/TeamGameMode.cpp
@@ -12,6 +12,30 @@ ATeamGameMode::ATeamGameMode()
 	GameStateClass = ALBlasterGameState::StaticClass();
 }
 
+void ATeamGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	// 도중에 참가한 플레이어는 팀원이 적은 팀으로 배치
+	if (ALBlasterGameState* LBGameState = GetGameState<ALBlasterGameState>())
+	{
+		if (ALBlasterPlayerState* PlayerState = NewPlayer->GetPlayerState<ALBlasterPlayerState>())
+		{
+			const int32 NumRedTeam = LBGameState->RedTeam.Num();
+			const int32 NumBlueTeam = LBGameState->BlueTeam.Num();
+
+			if (NumRedTeam <= NumBlueTeam)
+			{
+				PlayerState->SetTeam(ETeam::ET_RedTeam);
+			}
+			else
+			{
+				PlayerState->SetTeam(ETeam::ET_BlueTeam);
+			}
+		}
+	}
+}
+
 void ATeamGameMode::Logout(AController* Exiting)
 {
 	Super::Logout(Exiting);
