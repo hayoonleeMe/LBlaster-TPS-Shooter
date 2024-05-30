2ae9d9e81008a22fc7fbda74b72351eb3c8fab81
hayoonleeMe <htdobad@gmail.com>
2024-05-30 23:26:30 +0900

    팀 별 PlayerStart 클래스와 그 클래스를 상속받는 블루프린트 생성

---
+++ Source/LBlaster/PlayerStart/RedTeamPlayerStart.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "RedTeamPlayerStart.generated.h"
+
+UCLASS()
+class LBLASTER_API ARedTeamPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+
+};
