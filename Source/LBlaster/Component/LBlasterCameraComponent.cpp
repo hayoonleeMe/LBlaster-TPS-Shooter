9bb8bd0fda4c3cb307d7de6de2fc6e453bcd9f66
hayoonleeMe <htdobad@gmail.com>
2024-06-07 03:06:37 +0900

    Camera의 View를 업데이트하고 난 뒤 Penetration을 Prevent하도록 구현.

--- Source/LBlaster/Component/LBlasterCameraComponent.cpp
+++ Source/LBlaster/Component/LBlasterCameraComponent.cpp
@@ -134,6 +134,8 @@ void ULBlasterCameraComponent::UpdateView(float DeltaTime, FCameraView& OutView)
 	OutView.Rotation = PivotRotation;
 	OutView.ControlRotation = OutView.Rotation;
 	OutView.FieldOfView = CameraMode.FieldOfView;
+
+	UpdatePreventPenetration(OutView);
 }
 
 void ULBlasterCameraComponent::SetBlendWeight(float Weight)
@@ -163,6 +165,67 @@ void ULBlasterCameraComponent::UpdateBlending(float DeltaTime)
 
 	const float Exponent = CameraMode.BlendExponent > 0.f ? CameraMode.BlendExponent : 1.f;
 	BlendWeight = FMath::InterpEaseOut(0.f, 1.f, BlendAlpha, Exponent);
+}
+
+void ULBlasterCameraComponent::UpdatePreventPenetration(FCameraView& OutView)
+{
+	if (const AActor* PPActor = OwnerCharacter)
+	{
+		if (const UPrimitiveComponent* PPActorRootComponent = Cast<UPrimitiveComponent>(PPActor->GetRootComponent()))
+		{
+			// 자동으로 SafeLocation을 선택하려는 시도입니다. 이를 통해 조준 시 카메라의 이동을 줄일 수 있습니다.
+			// 우리의 카메라는 조준점이므로, 조준을 가능한 한 부드럽고 안정적으로 유지하고 싶습니다.
+			// 캡슐에서 조준 라인에 가장 가까운 지점을 선택합니다.
+			FVector ClosestPointOnLineToCapsuleCenter;
+			FVector SafeLocation = PPActor->GetActorLocation();
+			FMath::PointDistToLine(SafeLocation, OutView.Rotation.Vector(), OutView.Location, ClosestPointOnLineToCapsuleCenter);
+
+			// 안전 거리를 조정하여 높이를 조준 라인과 동일하게 하되, 캡슐 내에 위치하도록 합니다.
+			const float PushInDistance = PenetrationAvoidanceFeelers[0].Extent + CollisionPushOutDistance;
+			const float MaxHalfHeight = PPActor->GetSimpleCollisionHalfHeight() - PushInDistance;
+			SafeLocation.Z = FMath::Clamp(ClosestPointOnLineToCapsuleCenter.Z, SafeLocation.Z - MaxHalfHeight, SafeLocation.Z + MaxHalfHeight);
+
+			float DistanceSqr;
+			PPActorRootComponent->GetSquaredDistanceToCollision(ClosestPointOnLineToCapsuleCenter, DistanceSqr, SafeLocation);
+			// 라인 검사를 수행할 때 초기 침투를 방지하기 위해 캡슐 내부로 밀어 넣습니다.	
+			SafeLocation += (SafeLocation - ClosestPointOnLineToCapsuleCenter).GetSafeNormal() * PushInDistance;
+			
+			PreventCameraPenetration(PPActor, SafeLocation, OutView.Location);
+		}
+	}
+}
+
+void ULBlasterCameraComponent::PreventCameraPenetration(const AActor* PPActor, const FVector& SafeLocation, FVector& CameraLocation)
+{
+	if (PPActor)
+	{
+		FVector BaseRay = CameraLocation - SafeLocation;
+		FRotationMatrix BaseRayMatrix(BaseRay.Rotation());
+		FVector BaseRayLocalUp, BaseRayLocalFwd, BaseRayLocalRight;
+	
+		BaseRayMatrix.GetScaledAxes(BaseRayLocalFwd, BaseRayLocalRight, BaseRayLocalUp);
+		float DistBlockedPctThisFrame = 1.f;
+	
+		FCollisionQueryParams SphereParams;
+		SphereParams.AddIgnoredActor(PPActor);
+
+		for (const FPenetrationAvoidanceFeeler& Feeler : PenetrationAvoidanceFeelers)
+		{
+			FVector RotatedRay = BaseRay.RotateAngleAxis(Feeler.AdjustmentRot.Yaw, BaseRayLocalUp);
+			RotatedRay = RotatedRay.RotateAngleAxis(Feeler.AdjustmentRot.Pitch, BaseRayLocalRight);
+			FVector RayTarget = SafeLocation + RotatedRay;
+
+			FHitResult Hit;
+			if (GetWorld()->SweepSingleByChannel(Hit, SafeLocation, RayTarget, FQuat::Identity, ECC_Camera, FCollisionShape::MakeSphere(Feeler.Extent), SphereParams))
+			{
+				float NewBlockPct = ((Hit.Location - SafeLocation).Size() - CollisionPushOutDistance) / (RayTarget - SafeLocation).Size();
+				DistBlockedPctThisFrame = FMath::Min(NewBlockPct, DistBlockedPctThisFrame);
+			}
+		}
+
+		DistBlockedPctThisFrame = FMath::Clamp(DistBlockedPctThisFrame, 0.f, 1.f);
+		CameraLocation = SafeLocation + (CameraLocation - SafeLocation) * DistBlockedPctThisFrame;
+	}
 }
 
 void ULBlasterCameraComponent::UpdateForTarget()
