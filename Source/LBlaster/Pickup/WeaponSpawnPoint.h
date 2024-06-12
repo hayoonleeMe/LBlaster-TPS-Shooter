64d149cd112e407e68a609dd103729f265136cf9
hayoonleeMe <htdobad@gmail.com>
2024-06-13 02:31:23 +0900

    WeaponPickup을 스폰하는 WeaponSpawnPoint 추가 및 각 무기를 스폰하는 블루프린트 추가

---
+++ Source/LBlaster/Pickup/WeaponSpawnPoint.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup/PickupSpawnPoint.h"
+#include "WeaponSpawnPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LBLASTER_API AWeaponSpawnPoint : public APickupSpawnPoint
+{
+	GENERATED_BODY()
+
+	/*
+	 *	Spawn Pickup
+	 */
+	virtual void SpawnPickup() override;
+
+	void StartSpawnWeaponPickupTimer();
+};
