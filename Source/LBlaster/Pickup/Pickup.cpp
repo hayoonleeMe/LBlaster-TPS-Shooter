8a5e2659a76eb29c557c04e702e552bcd613d35d
hayoonleeMe <htdobad@gmail.com>
2024-06-13 02:29:05 +0900

    Pickup 클래스와 AmmoPickup 클래스 분리

--- Source/LBlaster/Pickup/Pickup.cpp
+++ Source/LBlaster/Pickup/Pickup.cpp
@@ -3,9 +3,7 @@
 
 #include "Pickup/Pickup.h"
 
-#include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "LBTypes/WeaponTypes.h"
 
 APickup::APickup()
 {
@@ -17,53 +15,4 @@ APickup::APickup()
 
 	// 모든 Actor 클래스에 상속되는 기본 USceneComponent
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
-
-	/* Mesh & Overlap Sphere */
-	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
-	OverlapSphere->SetupAttachment(RootComponent);
-	OverlapSphere->SetSphereRadius(90.f);
-	OverlapSphere->SetRelativeLocation(FVector(0.f, 0.f, 10.f));
-	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	OverlapSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
-	OverlapSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
-
-	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
-	PickupMesh->SetupAttachment(OverlapSphere);
-	PickupMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	PickupMesh->SetRelativeScale3D(FVector(5.f, 5.f, 5.f));
-	PickupMesh->SetRenderCustomDepth(true);
-	PickupMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_PURPLE);
 }
-
-void APickup::Destroyed()
-{
-	if (PickupSound)
-	{
-		UGameplayStatics::PlaySoundAtLocation(this, PickupSound, GetActorLocation());
-	}
-	
-	Super::Destroyed();
-}
-
-void APickup::Tick(float DeltaSeconds)
-{
-	Super::Tick(DeltaSeconds);
-
-	if (PickupMesh)
-	{
-		PickupMesh->AddWorldRotation(FRotator(0.f, BaseTurnRate * DeltaSeconds, 0.f));
-	}
-}
-
-void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-                              bool bFromSweep, const FHitResult& SweepResult)
-{
-}
-
-void APickup::BeginPlay()
-{
-	Super::BeginPlay();
-
-	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnSphereOverlap);
-}
-
