cc2b6a9da232c4ce232dabc033d0ebeb8f6cdb07
hayoonleeMe <htdobad@gmail.com>
2024-06-13 23:14:19 +0900

    WeaponSpawnPoint에서 로컬 캐릭터의 CombatComponent에 접근하기 위해 기존의 ILBCharacterPickupInterface 사용

--- Source/LBlaster/Interface/LBCharacterPickupInterface.h
+++ Source/LBlaster/Interface/LBCharacterPickupInterface.h
@@ -24,4 +24,6 @@ class LBLASTER_API ILBCharacterPickupInterface
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	virtual void PickupAmmo(EWeaponType InWeaponType, int32 InAmmoAmount) = 0;
+	
+	virtual void FindNearestOverlappingWeapon() = 0; 
 };
