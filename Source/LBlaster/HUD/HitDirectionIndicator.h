150e266048e077b5aa6d8701d64a92bf5c85080a
hayoonleeMe <htdobad@gmail.com>
2024-08-05 12:37:37 +0900

    Hit Direction Indicator 애셋 추가

---
+++ Source/LBlaster/HUD/HitDirectionIndicator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HUD/LBlasterUserWidget.h"
+#include "HitDirectionIndicator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LBLASTER_API UHitDirectionIndicator : public ULBlasterUserWidget
+{
+	GENERATED_BODY()
+
+};
